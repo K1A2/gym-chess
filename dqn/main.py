63b9c6ef4b807c6561d9c0809d1a6ca52504ec4b
K1A2 <jckim0307@gmail.com>
2023-06-05 13:11:22 +0900

    feat: 코드 분할

--- dqn/main.py
+++ dqn/main.py
@@ -1,21 +1,27 @@
+import os
+
 from trainer.dqn import TrainDqn
 
 import tensorflow.keras.layers as layers
 import tensorflow.keras as keras
+import tensorflow as tf
 
 import gym
 
 def create_q_model(dqn_trainer):
-    inputs = layers.Input(shape=(64, ))
+    inputs = layers.Input(shape=(64,))
     layer1 = layers.Dense(128, activation="relu")(inputs)
     layer2 = layers.Dense(128, activation="relu")(layer1)
     action = layers.Dense(dqn_trainer.num_actions, activation="linear")(layer2)
     return keras.Model(inputs=inputs, outputs=action)
 
 if __name__ == '__main__':
-    dqn_trainer = TrainDqn()
-    device = dqn_trainer.check_device()
+    dqn_trainer = TrainDqn(batch_size=256)
+
+    dqn_trainer.check_device(use_cpu_force=False)
 
     dqn_trainer.set_env('ml_chess_env:ChessGreedyEnv')
 
     dqn_trainer.set_models(create_q_model(dqn_trainer), create_q_model(dqn_trainer))
+    dqn_trainer.train()
+    dqn_trainer.evaluate()
--- dqn/trainer/dqn.py
+++ dqn/trainer/dqn.py
@@ -1,6 +1,12 @@
 import tensorflow as tf
+import tensorflow.keras as keras
 
 import gym
+
+import numpy as np
+
+import time
+from IPython.display import clear_output, display
 
 import logging
 import os
@@ -77,14 +83,16 @@ class TrainDqn:
         self.__logger.addHandler(streaming_handler)
 
     def check_device(self, use_cpu_force=False):
+        if use_cpu_force:
+            os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
         gpus = tf.config.list_physical_devices('GPU')
         if len(gpus) and not use_cpu_force:
             self.__logger.info(f'Use GPU\tname: {gpus[0].name}')
-            return gpus[0].name
+            self.device = gpus[0].name.replace('physical_device:', '')
         else:
             cpu = tf.config.list_physical_devices('CPU')[0]
-            self.__logger.info(f'Use CPU\tname: {cpu[0].name}')
-            return cpu.name
+            self.__logger.info(f'Use CPU\tname: {cpu.name}')
+            self.device = cpu.name.replace('physical_device:', '')
 
     def set_env(self, env):
         self.env = gym.make(env)
@@ -95,3 +103,166 @@ class TrainDqn:
         self.model = model
         self.target_model = target_model
         self.__logger.info(f'set models')
+
+    def __init_train_variables(self):
+        self.running_reward = 0
+        self.episode_count = 0
+        self.frame_count = 0
+        self.action_history = []
+        self.state_history = []
+        self.state_next_history = []
+        self.rewards_history = []
+        self.done_history = []
+        self.episode_reward_history = []
+
+        self.optimizer = keras.optimizers.Adam(learning_rate=self.learning_rate, clipnorm=1.0)
+        self.loss_function = keras.losses.Huber()
+
+    def __convert_state(self, board):
+        return np.array(board)
+
+    def __get_greedy_epsilon(self, state, mask):
+        if self.frame_count < self.epsilon_random_frames or self.epsilon > np.random.rand(1)[0]:
+            action = np.random.choice([i for i in range(self.num_actions) if mask[i] == 1])
+        else:
+            action_probs = self.model(np.expand_dims(state, 0), training=False)
+
+            valid_probs = [(i, action_probs[0][i]) for i in range(self.num_actions) if mask[i] == 1]
+            idx, val = max(valid_probs, key=lambda e: e[1])
+            action = np.random.choice([i for i, prob in valid_probs if prob >= val])
+
+        self.epsilon -= self.epsilon_interval / self.epsilon_greedy_frames
+        self.epsilon = max(self.epsilon, self.epsilon_min)
+
+        return action
+
+    def __get_greedy_action(self, state, mask):
+        action_probs = self.model(np.expand_dims(state, 0), training=False)
+
+        valid_probs = [(i, action_probs[0][i]) for i in range(self.num_actions) if mask[i] == 1]
+        idx, val = max(valid_probs, key=lambda e: e[1])
+        action = np.random.choice([i for i, prob in valid_probs if prob >= val])
+
+        return action
+
+    def __sample_batch(self, batch_size):
+        indices = np.random.choice(range(len(self.done_history)), size=batch_size)
+
+        state_sample = np.array([self.state_history[i] for i in indices])
+        state_next_sample = np.array([self.state_next_history[i] for i in indices])
+        rewards_sample = [self.rewards_history[i] for i in indices]
+        action_sample = [self.action_history[i] for i in indices]
+        done_sample = tf.convert_to_tensor(
+            [float(self.done_history[i]) for i in indices]
+        )
+
+        return state_sample, state_next_sample, rewards_sample, action_sample, done_sample
+
+    def train(self):
+        self.__init_train_variables()
+        model_save_path = '../models/'
+        folder_list = []
+        for d in os.listdir(model_save_path):
+            try:
+                folder_list.append(int(d))
+            except:
+                continue
+        folder_list = sorted(folder_list)
+        if folder_list:
+            model_save_path = os.path.join(model_save_path, str(folder_list[-1] + 1))
+        else:
+            model_save_path = os.path.join(model_save_path, '0')
+        os.makedirs(model_save_path)
+
+        start = time.time()
+        for _ in range(self.max_episodes):
+            board, info = self.env.reset()
+            state = self.__convert_state(board)
+            action_mask = info['action_mask']
+            episode_reward = 0
+
+            for timestep in range(1, self.max_steps_per_episode):
+                self.frame_count += 1
+
+                action = self.__get_greedy_epsilon(state, action_mask)
+
+                board, reward, done, _, info = self.env.step(action)
+                state_next = self.__convert_state(board)
+                action_mask = info['action_mask']
+
+                episode_reward += reward
+
+                self.action_history.append(action)
+                self.state_history.append(state)
+                self.state_next_history.append(state_next)
+                self.done_history.append(done)
+                self.rewards_history.append(reward)
+                state = state_next
+
+                if self.frame_count % self.update_after_actions == 0 and len(self.done_history) > self.batch_size:
+                    state_sample, state_next_sample, rewards_sample, action_sample, done_sample = \
+                        self.__sample_batch(self.batch_size)
+
+                    future_rewards = self.target_model.predict(state_next_sample, verbose=0)
+
+                    updated_q_values = rewards_sample + self.gamma * tf.reduce_max(future_rewards, axis=1)
+
+                    masks = tf.one_hot(action_sample, self.num_actions)
+
+                    with tf.GradientTape() as tape:
+                        q_values = self.model(state_sample)
+                        q_action = tf.reduce_sum(tf.multiply(q_values, masks), axis=1)
+                        loss = self.loss_function(updated_q_values, q_action)
+
+                    grads = tape.gradient(loss, self.model.trainable_variables)
+                    self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+
+                if self.frame_count % self.update_target_network == 0:
+                    self.target_model.set_weights(self.model.get_weights())
+                    self.__logger.info(f"model sync - running reward: {self.running_reward} at episode {self.episode_count}, frame count {self.frame_count}")
+
+                if len(self.rewards_history) > self.max_memory_length:
+                    del self.rewards_history[:1]
+                    del self.state_history[:1]
+                    del self.state_next_history[:1]
+                    del self.action_history[:1]
+                    del self.done_history[:1]
+
+                if done:
+                    break
+
+            self.episode_reward_history.append(episode_reward)
+            if len(self.episode_reward_history) > 100:
+                del self.episode_reward_history[:1]
+            running_reward = np.mean(self.episode_reward_history)
+
+            self.episode_count += 1
+
+            if self.episode_count % 10 == 0:
+                self.__logger.info(f"# episode = {self.episode_count}:\tavg. reward = {running_reward}\ttime = {time.time() - start}sec")
+                start = time.time()
+
+            if self.episode_count % 2500 == 0:
+                self.model.save(os.path.join(model_save_path, 'model.{}'.format(self.episode_count)))
+                self.target_model.save(os.path.join(model_save_path, 'target_model.{}'.format(self.episode_count)))
+
+        self.model.save(os.path.join(model_save_path, 'model.final'))
+        self.target_model.save(os.path.join(model_save_path, 'target_model.final'))
+
+    def evaluate(self):
+        board, info = self.env.reset()
+        done = False
+
+        for timestep in range(1, 80):
+            state = self.__convert_state(board)
+            action_mask = info['action_mask']
+
+            move = self.__get_greedy_action(state, action_mask)
+            board, reward, done, _, info = self.env.step(move)
+
+            clear_output()
+            display(self.env.render())
+            time.sleep(1)
+
+            if done:
+                break
